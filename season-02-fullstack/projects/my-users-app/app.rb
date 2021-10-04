fe351cad71a1f00a83424622ee1490fbc541b79e
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-10-04 16:45:00 +0600

    CodeBase has been moved to > my_user_model.rb .

--- season-02-fullstack/projects/my-users-app/app.rb
+++ season-02-fullstack/projects/my-users-app/app.rb
@@ -5,34 +5,3 @@ N O T E:
     All necessary info contained in README.md
 ...
 =end
-
-class User
-    def create(user_info)
-        uniq_user_id
-
-        return uniq_user_id # In INTEGER Data-Type  
-    end
-
-    def get(uniq_user_id)
-        
-        return # All info about > USER
-    end
-
-    def all(all_uniq_users_id)
-        
-        return # Hash of USERS
-    end
-
-    def update(uniq_user_id, attributes, value)
-        
-        return # Hash of USER
-    end
-
-    def destroy(uniq_user_id)
-        
-        return # The Updated DATABASE
-    end
-end
-
-# Остановился на написании Программного Кода
-# Lust update: 15:50 30/09/2021
