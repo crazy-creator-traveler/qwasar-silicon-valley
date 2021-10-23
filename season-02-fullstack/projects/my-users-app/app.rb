445d69b6d470e0c840b8224645482c9900805893
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-10-23 18:33:23 +0600

    Updates: 1)Created routes such as: -post /users; -get /users; -post /sign_in; -put /users; -delete /sign_out; delete /users;   2)Code Blocks|Logic were written for routes such as: -post /users; -get /users.

--- season-02-fullstack/projects/my-users-app/app.rb
+++ season-02-fullstack/projects/my-users-app/app.rb
@@ -4,4 +4,49 @@
 N O T E:
     All necessary info contained in README.md
 ...
+    params > Key variable
+                ==> Which contains recived data.
 =end
+
+require 'sinatra'
+require_relative "my_user_model.rb"
+
+set :port, 8090
+#set :bind, '0.0.0.0'
+
+post '/users' do # Creating New User
+    # p(params)
+    if(params.empty?)
+        puts("You didn't enter any data!")
+        return nil
+    else
+        user = User.new()
+        user.create(params) # puts(params)
+    end    
+end
+
+get '/users' do # Displaying All Users-Data > Except their password!
+    user = User.new()
+    data = user.all # "#{data}"
+                    # puts("#{data}") # Will not work! We can display in Browser OR Terminal! But not in both same time !
+    data.collect do |index|
+        data = index.except(:password) 
+        "#{data}\n"
+    end
+end
+
+post '/sign_in' do
+    # code...
+end
+
+put '/users' do
+    # code...
+end
+
+delete '/sign_out' do
+    # code...
+end
+
+delete '/users' do
+    # code...
+end
