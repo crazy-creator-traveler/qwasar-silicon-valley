165dc2841c245a655e90336d9af7f9e2b5af6ace
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-10-27 08:06:01 +0600

    Updates > Added Tester-Titles to be displayed when starting the Test.

--- season-02-fullstack/projects/my-users-app/gandalf.sh
+++ season-02-fullstack/projects/my-users-app/gandalf.sh
@@ -10,17 +10,20 @@
 HOST="localhost:8090"
 
 # TEST-1 > Creating New User
+echo -e "\n* * * * * * * * * * * * * * * TEST-1 * * * * * * * * * * * * * * *"
 curl -X POST -i $HOST/users -d "firstname=Luffi" -d "lastname=Monkey" -d "age=22" -d "email=my_email@gmail.com" -d "password=Luffi"
 
 # TEST-2 > Reading All Users-Data
+echo -e "\n\n* * * * * * * * * * * * * * * TEST-2 * * * * * * * * * * * * * * *"
 curl -i $HOST/users
 
-# TEST-3 > Sign in to the system using: -email && -password 
+# TEST-3 > Sign in to the system using: -email && -password
+echo -e "\n\n* * * * * * * * * * * * * * * TEST-3 * * * * * * * * * * * * * * *"
 curl -X POST -c cookies.txt -i $HOST/sign_in -d "email=my_email@gmail.com" -d "password=Luffi"
-echo -e "\n\n"
 
 # TEST-4 > Changing the password of an account > Provided that the user is already in the system
+echo -e "\n\n* * * * * * * * * * * * * * * TEST-4 * * * * * * * * * * * * * * *"
 curl -X PUT -b cookies.txt -i $HOST/users -d "email=my_email@gmail.com" -d "password=Monkey"
-echo
 
 # TEST-5 > ...
+echo -e "\n\n* * * * * * * * * * * * * * * TEST-5 * * * * * * * * * * * * * * *"
