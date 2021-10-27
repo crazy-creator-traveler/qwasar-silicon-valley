6cf1f6b7a7ecd666b3a0ccc0b02df70044f2b9a5
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-10-27 19:54:22 +0600

    Updates > Added new test: TEST-5.

--- season-02-fullstack/projects/my-users-app/gandalf.sh
+++ season-02-fullstack/projects/my-users-app/gandalf.sh
@@ -9,21 +9,27 @@
 
 HOST="localhost:8090"
 
-# TEST-1 > Creating New User
+# TEST-1 > Creating New User.
 echo -e "\n* * * * * * * * * * * * * * * TEST-1 * * * * * * * * * * * * * * *"
 curl -X POST -i $HOST/users -d "firstname=Luffi" -d "lastname=Monkey" -d "age=22" -d "email=my_email@gmail.com" -d "password=Luffi"
 
-# TEST-2 > Reading All Users-Data
+# TEST-2 > Reading All Users-Data.
 echo -e "\n\n* * * * * * * * * * * * * * * TEST-2 * * * * * * * * * * * * * * *"
 curl -i $HOST/users
 
-# TEST-3 > Sign in to the system using: -email && -password
+# TEST-3 > Sign in to the system using: -email && -password.
 echo -e "\n\n* * * * * * * * * * * * * * * TEST-3 * * * * * * * * * * * * * * *"
 curl -X POST -c cookies.txt -i $HOST/sign_in -d "email=my_email@gmail.com" -d "password=Luffi"
 
-# TEST-4 > Changing the password of an account > Provided that the user is already in the system
+# TEST-4 > Changing the password of an account > Provided that the user is already in the system.
 echo -e "\n\n* * * * * * * * * * * * * * * TEST-4 * * * * * * * * * * * * * * *"
 curl -X PUT -b cookies.txt -i $HOST/users -d "email=my_email@gmail.com" -d "password=Monkey"
 
-# TEST-5 > ...
+# TEST-5 > Logged out from the system > Provided that the user is already in the system.
 echo -e "\n\n* * * * * * * * * * * * * * * TEST-5 * * * * * * * * * * * * * * *"
+curl -X DELETE -b cookies.txt -i $HOST/sign_out
+> cookies.txt # To Delete the contents of the file.
+# rm cookies.txt
+
+# TEST-6 > ...
+echo -e "\n\n* * * * * * * * * * * * * * * TEST-6 * * * * * * * * * * * * * * *"
