21b17581cb9a01406b21c4603f5bce6ceb78e237
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-10-27 14:35:40 +0600

    Updates > Code Blocks|Logic were written for routes such as: -post /sign_in; -put /users.

--- season-02-fullstack/projects/my-users-app/app.rb
+++ season-02-fullstack/projects/my-users-app/app.rb
@@ -1,18 +1,25 @@
 =begin
-        Main Program Code
+        Main Program Code of the Controller-Block
 
 N O T E:
     All necessary info contained in README.md
-...
-    params > Key variable
-                ==> Which contains recived data.
+* * *
+    params > Key Variable (by default > Sinatra)
+                ==> Which contains received data > In Hash-Data type.
+EXAMPLE:
+    1) params   > Contain all received data
+    "#{params}" > To display all recived data.
+
+    2) params[:key_name]   > Contain certain received data
+    "#{params[:key_name]}" > To display Value of the Key_Name.
 =end
 
 require 'sinatra'
 require_relative "my_user_model.rb"
 
 set :port, 8090
-#set :bind, '0.0.0.0'
+# set :bind, '0.0.0.0'
+enable :sessions # To use SESSIONS
 
 post '/users' do # Creating New User
     # p(params)
@@ -31,16 +38,37 @@ get '/users' do # Displaying All Users-Data > Except their password!
                     # puts("#{data}") # Will not work! We can display in Browser OR Terminal! But not in both same time !
     data.collect do |index|
         data = index.except(:password) 
-        "#{data}\n"
+        "#{data}"
     end
 end
 
 post '/sign_in' do
-    # code...
+    #"#{params[:email]}"
+    users = User.new()
+    users = users.all
+
+    users.each do |user|
+        if(user[:email] == params[:email] && user[:password] == params[:password])
+            # p(user[:id]);   # p(session[:user_id])
+            session[:user_id] = user[:id] # puts(ssession)
+            message = "Welcome #{user[:firstname]}!" 
+            
+            return message
+        else
+            session[:user_id] = nil; 
+            # puts("User not exist") # To display in Terminal
+        end
+    end 
 end
 
 put '/users' do
-    # code...
+    if(session[:user_id])
+        user = User.new()
+        user = user.get(session[:user_id]) # p(session[:user_id])
+        "User #{user[:firstname]} is already Authorized !"
+    else
+        "User #{user[:firstname]} isn't Authorized !"
+    end
 end
 
 delete '/sign_out' do
