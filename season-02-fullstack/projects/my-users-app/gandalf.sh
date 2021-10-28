f739a1d73014df44641325eb343a7e1e7021e678
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-10-28 21:38:08 +0600

    Small changes were made in the description of Tests in such as: TEST-1; TEST-5.

--- season-02-fullstack/projects/my-users-app/gandalf.sh
+++ season-02-fullstack/projects/my-users-app/gandalf.sh
@@ -9,7 +9,7 @@
 
 HOST="localhost:8090"
 
-# TEST-1 > Creating New User.
+# TEST-1 > Creating a New User.
 echo -e "\n* * * * * * * * * * * * * * * TEST-1 * * * * * * * * * * * * * * *"
 curl -X POST -i $HOST/users -d "firstname=Luffi" -d "lastname=Monkey" -d "age=22" -d "email=my_email@gmail.com" -d "password=Luffi"
 
@@ -25,7 +25,7 @@ curl -X POST -c cookies.txt -i $HOST/sign_in -d "email=my_email@gmail.com" -d "p
 echo -e "\n\n* * * * * * * * * * * * * * * TEST-4 * * * * * * * * * * * * * * *"
 curl -X PUT -b cookies.txt -i $HOST/users -d "email=my_email@gmail.com" -d "password=Monkey"
 
-# TEST-5 > Logged out from the system > Provided that the user is already in the system.
+# TEST-5 > Sign out from the system > Provided that the user is already in the system.
 echo -e "\n\n* * * * * * * * * * * * * * * TEST-5 * * * * * * * * * * * * * * *"
 curl -X DELETE -b cookies.txt -i $HOST/sign_out
 > cookies.txt # To Delete the contents of the file.
