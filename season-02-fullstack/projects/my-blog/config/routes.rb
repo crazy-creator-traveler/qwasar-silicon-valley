1881ec211c0dfb11f725fc12cfbe3e2cf226d457
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-12-11 23:14:32 +0600

    Updates >> Added > the root route and specified an explicit view for it.

--- season-02-fullstack/projects/my-blog/config/routes.rb
+++ season-02-fullstack/projects/my-blog/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "articles#index" # root route == http://localhost:3000
   get "/articles", to: "articles#index" # to: ".." > Starts searching in the Views-Component 
                                         # for the required html.erb file.
 end
