26467b112ac309ea5de4acdee8e741fd95c95800
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-12-15 18:58:07 +0600

    Updates >> Changed > two routes: -get '/articles'; -get '/articles/:id' have been replaced by the resources method.

--- season-02-fullstack/projects/my-blog/config/routes.rb
+++ season-02-fullstack/projects/my-blog/config/routes.rb
@@ -1,7 +1,10 @@
+=begin
+NOTE:
+  to: ".."
+          ==> Starts searching in the Views-Component for the required html.erb file.
+=end
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "articles#index" # root route == http://localhost:3000
-  get "/articles", to: "articles#index" # to: ".." > Starts searching in the Views-Component 
-                                        # for the required html.erb file.
-  get "/articles/:id", to: "articles#show"
+  root "articles#index" # root route == http://localhost:3000                                        
+  resources :articles
 end
