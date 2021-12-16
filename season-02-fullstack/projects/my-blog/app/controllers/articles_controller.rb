f90ee4ef0dd06ddbcfd8d5fef1fa52144a50006c
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2021-12-16 10:41:18 +0600

    Updates >> Added >: new methods: -new(), -create() and necessary comments.

--- season-02-fullstack/projects/my-blog/app/controllers/articles_controller.rb
+++ season-02-fullstack/projects/my-blog/app/controllers/articles_controller.rb
@@ -1,3 +1,30 @@
+=begin
+  NOTE:
+    - Each Method in the ArticlesController Сlass is called > Action.
+
+    - By default, the name of the Method|Action and View > Must be identical.
+          ==> That is, the Method|Action must > Refer to app/views/articles/action_name.html.erb
+                                              > Have a view app/views/articles/action_name.html.erb
+Example:
+  Method|Action > index
+  View          > index.html.erb  
+  Route         > "http://localhost:3000/articles"
+
+  Method|Action > show
+  View          > show.html.erb
+  Route         > "http://localhost:3000/articles/id" 
+
+    - When an action does not explicitly render a view (or otherwise trigger an HTTP response)
+          ==> Rails will automatically render a view that matches the name of the controller and action. Convention Over Configuration!
+            ==> Views are located in the app/views directory.
+Example:
+  Controller    > articles_controller.rb
+  Method|Action > index
+
+  View          > articles
+                > index.html.erb
+                                  ==> articles/index.html.erb  
+=end
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
@@ -5,5 +32,22 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create 
+    @article = Article.new(title: "...", body: "...")
+      if @article.save
+        # If the article is saved successfully
+        # the action redirects the browser to the article's page at "http://localhost:3000/articles/#{@article.id}"
+        redirect_to @article
+      else
+        # The action redisplays the form by rendering app/views/articles/new.html.erb
+        # with a status code 4XX for the app to work fine with Turbo.
+        render :new, status: :unprocessable_entity
+      end
   end
 end
