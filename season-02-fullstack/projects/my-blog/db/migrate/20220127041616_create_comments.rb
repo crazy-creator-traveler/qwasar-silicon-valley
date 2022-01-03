fd60bc9b11abe294e3ca49eb752b9b6177f21cf5
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-01-03 15:31:25 +0600

    Updates >> Added > necessary comments to the line 't.references ..'.

--- season-02-fullstack/projects/my-blog/db/migrate/20220127041616_create_comments.rb
+++ season-02-fullstack/projects/my-blog/db/migrate/20220127041616_create_comments.rb
@@ -3,7 +3,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.string :commenter
       t.text :body
-      t.references :article, null: false, foreign_key: true
+      t.references :article, null: false, foreign_key: true # creates an integer column called article_id,
+                                                            # an index for it, and a foreign key constraint that points to the id column of the articles table.
 
       t.timestamps
     end
