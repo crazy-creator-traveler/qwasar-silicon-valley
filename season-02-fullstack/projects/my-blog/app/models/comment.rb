1374ab37a094ac8f9b3312f4d0858d8509cefb03
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-01-04 10:01:02 +0600

    Updates >> Added > necessary comments about the Active Record Association.

--- season-02-fullstack/projects/my-blog/app/models/comment.rb
+++ season-02-fullstack/projects/my-blog/app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :article # sets up an Active Record association.
+# Sets up an Active Record Association
+# Active Record Associations let us easily declare the relationship between two models:
+#   -comment
+# and
+#   -article
+# in this case we specify that > Each comment belongs to one article.
+# NOTE:
+#   For more info, about the Active Record Associations > https://guides.rubyonrails.org/v6.1/association_basics.html  
+  belongs_to :article
 end
