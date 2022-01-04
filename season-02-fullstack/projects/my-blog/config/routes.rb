6970a6ed4953466e17ae46fca08451c716f12357
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-01-04 13:01:49 +0600

    Updates >> Added > necessary comments about the nested resources within 'resources :articles'.

--- season-02-fullstack/projects/my-blog/config/routes.rb
+++ season-02-fullstack/projects/my-blog/config/routes.rb
@@ -6,7 +6,12 @@ NOTE:
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "articles#index" # root route == http://localhost:3000                                        
+  
   resources :articles do
+    # This creates comments as a nested resource within articles
+    # and thus we automatically get all the necessary paths for CRUD operations to work with comments.
+    # NOTE:
+    #   For more information about routing, see the Rails Routing guide > https://guides.rubyonrails.org/v6.1/routing.html#nested-resources
     resources :comments
   end
 end
