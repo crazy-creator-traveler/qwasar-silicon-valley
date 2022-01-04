1f9030b8a18ab86025461f8b1ec1ab3e29f29d63
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-01-04 10:32:05 +0600

    Updates >> Added > routes for CRUD operations to work with article comments.

--- season-02-fullstack/projects/my-blog/config/routes.rb
+++ season-02-fullstack/projects/my-blog/config/routes.rb
@@ -6,5 +6,7 @@ NOTE:
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "articles#index" # root route == http://localhost:3000                                        
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
