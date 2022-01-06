d0a881496944991bed9214e8e3e3f19fe1baff53
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-01-06 11:11:55 +0600

    Updates >> Added > necessary comments about the create() method.

--- season-02-fullstack/projects/my-blog/app/controllers/comments_controller.rb
+++ season-02-fullstack/projects/my-blog/app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
+  # We'll see a bit more complexity here than we did in the controller for articles.
+  # That's a side-effect of the nesting that we've set up.
   def create
     @article = Article.find(params[:article_id])
+
+    # We use the create method on @article.comments to create and save the comment.
+    # This will automatically link the comment so that it belongs to that particular article
     @comment = @article.comments.create(comment_params)
   
+    # article_path(@article) helper > As we have already seen,
+    # calls the show action of the ArticlesController which in turn renders the show.html.erb template.
     redirect_to article_path(@article)
   end
 
