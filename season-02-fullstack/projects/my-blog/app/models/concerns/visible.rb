87e733f8aaf28384e529d77baeb12c46a13bdece
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-01-12 11:01:38 +0600

    Updates >> Created > public_count class method.

--- season-02-fullstack/projects/my-blog/app/models/concerns/visible.rb
+++ season-02-fullstack/projects/my-blog/app/models/concerns/visible.rb
@@ -7,6 +7,12 @@ module Visible
     validates :status, inclusion: { in: VALID_STATUSES }
   end
 
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
   def archived?
     status == 'archived'
   end   
