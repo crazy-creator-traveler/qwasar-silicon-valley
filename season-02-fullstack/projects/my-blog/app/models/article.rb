61bf4476b156dcb7a3f67f91ed0feae4c62dccab
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-01-17 13:43:34 +0600

    Updates >> Added > necessary comments about the validations.

--- season-02-fullstack/projects/my-blog/app/models/article.rb
+++ season-02-fullstack/projects/my-blog/app/models/article.rb
@@ -1,4 +1,13 @@
+=begin
+NOTE:
+  Active Record automatically defines model attributes for every table column
+    > So we don't have to declare those attributes in our model file.
+=end
 class Article < ApplicationRecord
+# Validations are rules that are checked before a model object is saved. 
+# If any of the checks fail:
+#   1)The save will be aborted
+#   2)Appropriate error messages will be added to the errors attribute of the model object.
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
