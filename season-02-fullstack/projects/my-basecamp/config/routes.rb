79a2bb436b53a9f59534a6613b5c2be6dfac7eba
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-03-16 17:24:41 +0600

    1)Updated >> routes handled by the 'devise/passwords' controller were rewritten;   2)Added >> additional argument to the devise_for method so that the method doesn't create routes for the 'devise/passwords' controller.

--- season-02-fullstack/projects/my-basecamp/config/routes.rb
+++ season-02-fullstack/projects/my-basecamp/config/routes.rb
@@ -10,13 +10,21 @@ Rails.application.routes.draw do
       get "/sing-in", to: "devise/sessions#new", as: :new_user_session
       post "/sing-in", to: "devise/sessions#create", as: :user_session
       delete "/sign-out", to: "devise/sessions#destroy", as: :destroy_user_session
+
+      scope "/password" do
+        get "/new", to: "devise/passwords#new", as: :new_user_password
+        get "/edit", to: "devise/passwords#edit", as: :edit_user_password
+        patch "/", to: "devise/passwords#update", as: :user_password
+        put "/", to: "devise/passwords#update"
+        post "/", to: "devise/passwords#create"
+      end
     end
   end
 
-  devise_for :users, skip: :sessions
+  devise_for :users, skip: [:sessions, :password ]
 
   get "/how-it-works", to: "home_pages#how_it_works"
-  
+
   get "/before-and-after", to: "home_pages#before_and_after"
 
   get "/features", to: "home_pages#features"
