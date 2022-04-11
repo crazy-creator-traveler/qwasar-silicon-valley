3b7125637e3496d083a0283ead26284f4fdba628
SULTAN KYZYKTAU <traveler.in.the.world.sphere.inf@gmail.com>
2022-04-11 19:31:02 +0600

    Changed >> controllers that handled routes created by the 'devise' gem by default.

--- season-02-fullstack/projects/my-basecamp/config/routes.rb
+++ season-02-fullstack/projects/my-basecamp/config/routes.rb
@@ -19,25 +19,25 @@ Rails.application.routes.draw do
   devise_scope :user do
     scope "/user" do
       get "/", to: "users/home_pages#index", as: :user_root
-      get "/sing-in", to: "devise/sessions#new", as: :new_user_session
-      post "/sing-in", to: "devise/sessions#create", as: :user_session
-      delete "/sign-out", to: "devise/sessions#destroy", as: :destroy_user_session
+      get "/sing-in", to: "users/sessions#new", as: :new_user_session
+      post "/sing-in", to: "users/sessions#create", as: :user_session
+      delete "/sign-out", to: "users/sessions#destroy", as: :destroy_user_session
 
       scope "/password" do
-        get "/new", to: "devise/passwords#new", as: :new_user_password
-        get "/edit", to: "devise/passwords#edit", as: :edit_user_password
-        patch "/", to: "devise/passwords#update", as: :user_password
-        put "/", to: "devise/passwords#update"
-        post "/", to: "devise/passwords#create"
+        get "/new", to: "users/passwords#new", as: :new_user_password
+        get "/edit", to: "users/passwords#edit", as: :edit_user_password
+        patch "/", to: "users/passwords#update", as: :user_password
+        put "/", to: "users/passwords#update"
+        post "/", to: "users/passwords#create"
       end
 
-      get "/cancel", to: "devise/registrations#cancel", as: :cancel_user_registration
-      get "/sign-up", to: "devise/registrations#new", as: :new_user_registration
-      get "/account", to: "devise/registrations#edit", as: :edit_user_registration
-      patch "/", to: "devise/registrations#update", as: :user_registration
-      put "/", to: "devise/registrations#update"
-      delete "/", to: "devise/registrations#destroy"
-      post "/", to: "devise/registrations#create"
+      get "/cancel", to: "users/registrations#cancel", as: :cancel_user_registration
+      get "/sign-up", to: "users/registrations#new", as: :new_user_registration
+      get "/account", to: "users/registrations#edit", as: :edit_user_registration
+      patch "/", to: "users/registrations#update", as: :user_registration
+      put "/", to: "users/registrations#update"
+      delete "/", to: "users/registrations#destroy"
+      post "/", to: "users/registrations#create"
     end
   end
 
